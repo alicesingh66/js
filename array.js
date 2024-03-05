35b4995c22cc6d603b41f5fa8f24471de1eb208f
Alice Singh <145307137+alicesingh66@users.noreply.github.com>
2024-03-05 21:47:23 +0530

    Array part1

---
+++ array.js
@@ -0,0 +1,24 @@
+//ARRAYS
+
+const myArr = [3, 5, 4, 56, 67, 34, 58, 32, 234];
+// const array2 = ["harry", "James"];
+// const myArr2 = new Array(1, 2, 3, 4);
+// console.log(myArr2[2]);
+
+//Array Methods
+
+// myArr.push(4);
+// myArr.pop();
+// myArr.unshift(8);
+// myArr.shift();
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(19));
+
+// console.log(typeof myArr.join());
+// console.log(myArr);
+
+// slice splice
+console.log("A", myArr);
+console.log(`B array is [${myArr.slice(0, 3)}]`);
+
+console.log(`B array is [${myArr.splice(0, 3)}]`);
