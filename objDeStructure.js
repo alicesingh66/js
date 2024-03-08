a2ae0ec83912dfed3028095608410d1d3271087e
Alice Singh <145307137+alicesingh66@users.noreply.github.com>
2024-03-08 17:07:21 +0530

    JSON intro

---
+++ objDeStructure.js
@@ -0,0 +1,26 @@
+//Object discussion--> APIs discussion
+//Destructuring of Objects and array
+//Destructure = Destructuring" typically refers to the process of extracting values from data structures like arrays or objects in programming languages. It allows you to unpack values from these structures into distinct variables, making it more convenient to work with them.
+
+const course = {
+  name: "jsHindi",
+  price: 999,
+  instructor: "Hitesh",
+};
+
+//course.instructor
+
+const { instructor: tecaher } = course;
+console.log(tecaher);
+//the obtained values are stored in teacher, smaller better name
+
+// const navbar = ()
+
+//API
+//XML-->JSON
+
+// {
+//   "name":'Alice',
+//   "courseName": 'LearningJS',
+//   "price": "free"
+// }
