c5a33af0dd8f28ab4dd2f3b8001807bbf9fc0d02
Alice Singh <145307137+alicesingh66@users.noreply.github.com>
2024-03-14 22:13:43 +0530

    switch

---
+++ switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("Jan");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("Mar");
+    break;
+  case 4:
+    console.log("Apr");
+    break;
+  case 5:
+    console.log("May");
+    break;
+
+  default:
+    console.log("Not first half of month");
+    break;
+}
