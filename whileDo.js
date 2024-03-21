1a26bd7108ed8dc409ea62ba8c837efc01eb4e49
Alice Singh <145307137+alicesingh66@users.noreply.github.com>
2024-03-21 21:16:59 +0530

    Add files via upload

---
+++ whileDo.js
@@ -0,0 +1,21 @@
+//whileDo loop
+let index = 0;
+while (index <= 10) {
+  //console.log(`Value of index is ${index}`);
+  index = index + 2;
+}
+
+let myArray = ["a", "b", "c", "d"];
+let arr = 0;
+while (arr < myArray.length) {
+  //console.log(`Array value ${myArray[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 11;
+
+do {
+  console.log(`score is ${score}`);
+  score++;
+} while (score <= 10);
+//work -->then condition
